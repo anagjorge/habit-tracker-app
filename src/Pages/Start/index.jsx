7dc941b0592534e84aa1026e883a8d230b70d8a9
anajorge <anajessicadegj@gmail.com>
2022-12-02 16:05:34 -0300

    inserting animations

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,19 +1,45 @@
 import React from "react";
-import { View, ScrollView, Text, Image } from "react-native";
+import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
+
+import LifeStatus from "../../Components/Common/LifeStatus";
 
 export default function Start() {
     return (
-        <View>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View>
-                    <Image
-                        source={require("../../assets/icons/logo3.png")}
+        <View style={styles.container}>
+            <ScrollView  style={styles.scrool} showsVerticalScrollIndicator={false}>
+                <View style={{ alignItems: "center" }}>
+                    <Image 
+                    source={require("../../assets/icons/logo3.png")} 
+                    style={styles.logo}
                     />
-                    <Text>
-                        Vamos transformar sua vida {"/n"} em jogo, buscando sempre {"/n"} o melhor nível
+                    <LifeStatus/>
+                    <Text style={styles.description}>
+                        Vamos transformar sua vida {"\n"} em um jogo, buscando sempre {"\n"}{" "}
+                        o melhor nível.
                     </Text>
                 </View>
             </ScrollView>
         </View>
-    )
+    );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "rgba(21, 21, 21, 0.98)",
+    },
+    logo: {
+        width: 300, 
+        height: 60,
+        marginTop: 60,
+        marginBottom: 20,
+    },
+    description: {
+        color: "#FFFFFF",
+        fontSize: 20,
+        textAlign: "center",
+        marginVertical: 60,
+    },
+
+});
